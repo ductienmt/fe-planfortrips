dd29011bb76a7954029c9dbd464d5c09d4712f25
HuynhAnhQuan2 <nguyennam12345132@gmail.com>
2024-10-17 19:53:21 +0700

    Plan da chinh sua cai link

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-import Hotel from "./pages/Client/Hotel/Hotel.jsx"
-import HotelInfo from "./pages/Client/Hotel/HotelDetails/HotelInfo.jsx";
-import HotelDetails from "./pages/Client/Hotel/HotelDetails/HotelDetails.jsx";
-import BookingHotelHotelInfo from "./pages/Client/Hotel/BookingHotel/BookingHotelHotelInfo.jsx";
-import BookingHotel from "./pages/Client/Hotel/BookingHotel/BookingHotel.jsx";
+import App from "./App";
+import ErrorBoundary from "antd/es/alert/ErrorBoundary";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BookingHotel />
-  </StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Router>
+  </React.StrictMode>
 );
