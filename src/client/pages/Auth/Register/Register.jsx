0651c63ddf93658bcf2023bea3adbcc3d59a7286
Đinh Tăng Đức Tiến <100287902+ductienmt@users.noreply.github.com>
2024-10-19 12:57:23 +0700

    edit css

--- src/client/pages/Auth/Register/Register.jsx
+++ src/client/pages/Auth/Register/Register.jsx
@@ -48,7 +48,7 @@ const Register = () => {
   };
 
   return (
-    <section className="vh-100">
+    <section className="vh-100 register-container">
       <div className="container-fluid h-custom">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-7 d-flex justify-content-center position-relative">
