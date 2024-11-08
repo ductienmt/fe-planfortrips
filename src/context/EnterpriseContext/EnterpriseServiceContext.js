21dd23cb112447db632f3cd3e3f2d37c067a64cb
Đinh Tăng Đức Tiến <100287902+ductienmt@users.noreply.github.com>
2024-11-09 03:08:21 +0700

    commit login

--- src/context/EnterpriseContext/EnterpriseServiceContext.js
+++ src/context/EnterpriseContext/EnterpriseServiceContext.js
@@ -1,4 +1,4 @@
-class TypeEnterprise {
+class Enterprise {
   addTypeEnterprise(typeEnterprise) {
     sessionStorage.setItem("typeEnterprise", typeEnterprise);
   }
---
+++ src/enterprise/accomodation/roomHistory/RoomHistory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RoomHistory.css";
+
+const RoomHistory = () => {
+  return (
+    <>
+      <div>RoomHistory</div>
+    </>
+  );
+};
+
+export default RoomHistory;
