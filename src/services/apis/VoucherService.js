b2d8b83ddf2ccd773ff74160dbedc0d2318d0b8c
Đinh Tăng Đức Tiến <100287902+ductienmt@users.noreply.github.com>
2024-11-23 19:51:45 +0700

    dang lam room voucher

--- src/services/apis/VoucherService.js
+++ src/services/apis/VoucherService.js
@@ -1,9 +1,31 @@
 import Http from "../Http";
 
 export const VoucherService = {
-  getVoucherByEnterpriseId: async (page, limit, keywords) => {
+  getVoucherByEnterpriseId: async (
+    pageNo,
+    pageSize,
+    status,
+    sortBy,
+    sortType
+  ) => {
+    const params = new URLSearchParams();
+    if (pageNo) {
+      params.append("pageNo", pageNo);
+    }
+    if (pageSize) {
+      params.append("pageSize", pageSize);
+    }
+    if (status) {
+      params.append("status", status);
+    }
+    if (sortBy) {
+      params.append("sortBy", sortBy);
+    }
+    if (sortType) {
+      params.append("sortType", sortType);
+    }
     const response = await Http.get(
-      `api/v1/coupons/all?page=${page}&limit=${limit}&keywords=${keywords}`
+      `api/v1/coupons/getByEnterpriseId?${params.toString()}`
     );
     return response.data;
   },
