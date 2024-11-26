7c3d16ec568dee55e44b4fddfe8dd163d9a9b2f5
haonam1212 <namhhps31884@fpt.edu.vn>
2024-11-26 17:59:39 +0700

    fix thay đổi vé, nơi ở,...

--- src/client/pages/Hotel/roomCard/roomCard.jsx
+++ src/client/pages/Hotel/roomCard/roomCard.jsx
@@ -1,17 +1,18 @@
-import img1 from "../../../../assets/beach.jpg";
-import img3 from "../../../../assets/beach.jpg";
-import img5 from "../../../../assets/beach.jpg";
+import { convertToVND } from "../../../../utils/FormatMoney";
 import "./roomCard.css";
 import { Link } from "react-router-dom";
 
-const RoomCard = () => {
+const RoomCard = ({ img, roomSize, priceOneNight, onBook }) => {
   return (
     <>
       <div className="flex-container-room-card">
+
+
+
         <div className="card-room-type align-items-center">
           <div className="d-flex align-items-center custom-img-room-type">
-            <img style={{ width: "500px", }} src={img1} alt="" />
-            <p className="content-type ms-3">Phòng đơn 1 người</p>
+            <img style={{ width: "500px" }} src={img} alt="" />
+            <p className="content-type ms-3">{roomSize}</p>
           </div>
           <div className="price-room-type text-end">
             <p className="amenities d-flex justify-content-center mt-3">
@@ -25,52 +26,16 @@ const RoomCard = () => {
                 <i className="fa-solid fa-toilet-paper"></i>Dụng cụ vệ sinh
               </small>
             </p>
-            <span>200.000 VNĐ / 1 đêm</span>
-            <Link to="/hotel/booking">Đặt ngay</Link>
-          </div>
-        </div>
-        <div className="card-room-type align-items-center">
-          <div className="d-flex align-items-center custom-img-room-type">
-            <img style={{ width: "100%" }} src={img3} alt="" />
-            <p className="content-type ms-3">Phòng đôi 2 người</p>
-          </div>
-          <div className="price-room-type text-end">
-            <p className="amenities d-flex justify-content-center mt-3">
-              <small>
-                <i className="fa-solid fa-bowl-food"></i>Nước và đồ ăn
-              </small>
-              <small>
-                <i className="fa-solid fa-wifi"></i>Wifi
-              </small>
-              <small>
-                <i className="fa-solid fa-toilet-paper"></i>Dụng cụ vệ sinh
-              </small>
-            </p>
-            <span>300.000 VNĐ / 1 đêm</span>
-            <Link to="/hotel/booking">Đặt ngay</Link>
-          </div>
-        </div>
-        <div className="card-room-type align-items-center">
-          <div className="d-flex align-items-center custom-img-room-type">
-            <img style={{ width: "" }} src={img5} alt="" className="" />
-            <p className="content-type ms-3">
-              Phòng gia đình (2 người lớn, 2 bé nhỏ)
-            </p>
-          </div>
-          <div className="price-room-type text-end">
-            <p className="amenities d-flex justify-content-center mt-3">
-              <small>
-                <i className="fa-solid fa-bowl-food"></i>Nước và đồ ăn
-              </small>
-              <small>
-                <i className="fa-solid fa-wifi"></i>Wifi
-              </small>
-              <small>
-                <i className="fa-solid fa-toilet-paper"></i>Dụng cụ vệ sinh
-              </small>
-            </p>
-            <span>400.000 VNĐ / 1 đêm</span>
-            <Link to={"./booking"}>Đặt ngay</Link>
+            <span>{convertToVND(priceOneNight)} / 1 đêm</span>
+            <button
+              type="button"
+              className="book-ticket-transport"
+              // data-bs-toggle="modal"
+              // data-bs-target="#bookingRoomModal"
+              onClick={onBook}
+            >
+              Đặt ngay
+            </button>
           </div>
         </div>
       </div>
